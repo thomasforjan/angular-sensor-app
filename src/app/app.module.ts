261c8c2f4779921af22cea12359cf0e822327230
Thomas Forjan <2110859004@fh-burgenland.at>
2022-11-11 17:08:35 +0100

    Update: app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from './header/header.component';
+import { SensorsDataComponent } from './dashboard/sensors-data/sensors-data.component';
+import { AddSensorsDataComponent } from './dashboard/add-sensors-data/add-sensors-data.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ButtonComponent } from './button/button.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DeleteDialogComponent } from './dialogs/delete-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    SensorsDataComponent,
+    AddSensorsDataComponent,
+    DashboardComponent,
+    AboutPageComponent,
+    DeleteDialogComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
