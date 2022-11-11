c66de4721d754cfe1646847735e386b83709b103
Thomas Forjan <2110859004@fh-burgenland.at>
2022-11-11 17:03:58 +0100

    Add: Angular-Material + own material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,22 @@
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 @NgModule({
-  exports: [],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
 })
 export class MaterialModule {}
