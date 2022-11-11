be516842a88a1954d136ba14692ffc99d6936d14
Thomas Forjan <2110859004@fh-burgenland.at>
2022-11-11 18:38:56 +0100

    Add: snackBar

--- src/app/dashboard/add-sensors-data/add-sensors-data.component.ts
+++ src/app/dashboard/add-sensors-data/add-sensors-data.component.ts
@@ -1,37 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { BackendService } from 'src/app/shared/backend.service';
 import { StoreService } from 'src/app/shared/store.service';
 
 @Component({
   selector: 'app-add-sensors-data',
   templateUrl: './add-sensors-data.component.html',
-  styleUrls: ['./add-sensors-data.component.scss']
+  styleUrls: ['./add-sensors-data.component.scss'],
 })
 export class AddSensorsDataComponent implements OnInit {
-
-  constructor(public storeService: StoreService, private formBuilder: UntypedFormBuilder, public backendService: BackendService) { }
+  constructor(
+    public storeService: StoreService,
+    private formBuilder: UntypedFormBuilder,
+    public backendService: BackendService,
+    private _snackBar: MatSnackBar
+  ) {}
   public sensorenDataForm: any;
   public showAddTask: boolean = false;
 
   ngOnInit(): void {
     this.sensorenDataForm = this.formBuilder.group({
-      sensorId: [0, [ Validators.required ] ],
-      temperature: ['', [ Validators.required ] ],
-      humidity: ['', [ Validators.required ] ],
-      date:  [null, [ Validators.required ] ]
+      sensorId: [0, [Validators.required]],
+      temperature: ['', [Validators.required]],
+      humidity: ['', [Validators.required]],
+      date: [null, [Validators.required]],
     });
   }
 
   async onSubmit() {
-    if(this.sensorenDataForm?.valid) {
+    if (this.sensorenDataForm?.valid) {
       await this.backendService.addSensorsData(this.sensorenDataForm.value);
       this.sensorenDataForm.reset();
+      this._snackBar.open('Neuer Messerwert hinzugefügt', 'Ok', {
+        horizontalPosition: 'end',
+        verticalPosition: 'bottom',
+        duration: 2000,
+      });
     }
   }
 
   toggleAddTask() {
     this.showAddTask = !this.showAddTask;
   }
-
 }
