20f843ea1454cb4c2feecae22fc63c4b910d4b2f
Thomas Forjan <2110859004@fh-burgenland.at>
2022-11-12 11:14:40 +0100

    Add: snackBar for deleting sensor

--- src/app/dialogs/delete-dialog.component.ts
+++ src/app/dialogs/delete-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { BackendService } from '../shared/backend.service';
 
 @Component({
@@ -11,12 +12,18 @@ export class DeleteDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<DeleteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private backendService: BackendService
+    private backendService: BackendService,
+    private _snackBar: MatSnackBar
   ) {}
 
   confirmDeleteModal() {
     this.backendService.deleteSensorsDaten(this.data.id);
     this.dialogRef.close();
+    this._snackBar.open('Messerwert wird gelöscht', 'Ok', {
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+      duration: 2000,
+    });
   }
 
   cancelDeleteModal() {
